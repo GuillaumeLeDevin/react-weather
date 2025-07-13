d84398d5187613418ba154060ef1890f8223b7a4
Guillaume Le Devin <guillaume.le.devin@gmail.com>
2025-07-13 23:08:37 +0100

    first commit

---
+++ src/Local/Local.jsx
@@ -0,0 +1,14 @@
+export default function Local(weatherData) {
+  return (
+    <div>
+        <p className="city-name">{weatherData.city}</p>
+        <p className="country-name">{weatherData.country}</p>
+        <p className="temperature">{weatherData.temperature}°</p>
+        <div className="info-icon-container">
+            <img
+                className="info-icon"
+                src={`./icons/${weatherData.iconId}.svg`} alt="" />
+        </div>
+    </div>
+  )
+}
